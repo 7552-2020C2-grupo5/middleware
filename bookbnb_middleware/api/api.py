d068b9561cdbd0858e6e07d04739c3b5135298b1
Leonardo <lbellaera@fi.uba.ar>
2020-11-14 18:55:14 -0300

    Cambio minimo

--- bookbnb_middleware/api/api.py
+++ bookbnb_middleware/api/api.py
@@ -1,18 +1,18 @@
 import logging
 
 from flask_restx import Api
-from bookbnb_middleware import settings
 
 log = logging.getLogger(__name__)
 
-api = Api(version='1.0', title='BookBNB Middleware API',
-          description='BookBNB Middleware API for integrating backend microservices')
+api = Api(
+    version='1.0',
+    title='BookBNB Middleware API',
+    description='BookBNB Middleware API for integrating backend microservices',
+)
 
 
 @api.errorhandler
 def default_error_handler(e):
     message = 'An unhandled exception occurred.'
     log.exception(message)
-
-    if not settings.FLASK_DEBUG:
-        return {'message': message}, 500
+    return {'message': message}, 500
