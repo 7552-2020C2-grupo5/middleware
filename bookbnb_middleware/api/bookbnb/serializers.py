b405d847ed79e44910a4ff0c75f6931729c5f6fc
Leonardo <lbellaera@fi.uba.ar>
2020-11-14 22:13:37 -0300

    Mini refactor

--- bookbnb_middleware/api/bookbnb/serializers.py
+++ bookbnb_middleware/api/bookbnb/serializers.py
@@ -1,8 +1,11 @@
 from flask_restx import fields
 from bookbnb_middleware.api.api import api
 
-user_post = api.model('User post', {
-    'name': fields.Integer(required=True, description='User name'),
-    'last_name': fields.String(required=True, description='User last name'),
-    'mail': fields.String(required=True, description='User mail'),
-})
+user_post = api.model(
+    'User post',
+    {
+        'name': fields.String(required=True, description='User name'),
+        'last_name': fields.String(required=True, description='User last name'),
+        'mail': fields.String(required=True, description='User mail'),
+    },
+)
