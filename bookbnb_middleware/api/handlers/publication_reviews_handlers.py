6e972877b23714955236d7a5f504d85af582c24e
Leonardo <lbellaera@fi.uba.ar>
2021-03-04 04:11:04 -0300

    Fix

--- bookbnb_middleware/api/handlers/publication_reviews_handlers.py
+++ bookbnb_middleware/api/handlers/publication_reviews_handlers.py
@@ -3,25 +3,30 @@ import json
 import requests
 
 from bookbnb_middleware.constants import PUBLICATION_REVIEWS_URL
+from bookbnb_middleware.utils import get_sv_auth_headers
 
 headers = {"content-type": "application/json"}
 
 
 def list_publications_reviews(params):
-    r = requests.get(PUBLICATION_REVIEWS_URL + "/reviews", params=params)
+    h = get_sv_auth_headers()
+    r = requests.get(PUBLICATION_REVIEWS_URL + "/reviews", params=params, headers=h)
     return r.json(), r.status_code
 
 
 def create_publication_review(payload):
+    h = get_sv_auth_headers()
+    h.update(headers)
     r = requests.post(
-        PUBLICATION_REVIEWS_URL + "/reviews", data=json.dumps(payload), headers=headers
+        PUBLICATION_REVIEWS_URL + "/reviews", data=json.dumps(payload), headers=h
     )
     return r.json(), r.status_code
 
 
 def get_publication_score(publication_id):
+    h = get_sv_auth_headers()
     r = requests.get(
-        PUBLICATION_REVIEWS_URL + "/score/publication/" + str(publication_id)
+        PUBLICATION_REVIEWS_URL + "/score/publication/" + str(publication_id), headers=h
     )
     if r.status_code != 200:
         return None, r.status_code
--- bookbnb_middleware/api/handlers/questions_handlers.py
+++ bookbnb_middleware/api/handlers/questions_handlers.py
@@ -1,21 +1,25 @@
 import json
-
 import requests
 
 from bookbnb_middleware.constants import PUBLICATIONS_URL
+from bookbnb_middleware.utils import get_sv_auth_headers
 
 headers = {"content-type": "application/json"}
 
 
 def create_question(publication_id, payload):
+    h = get_sv_auth_headers()
+    h.update(headers)
     url = PUBLICATIONS_URL + "/" + str(publication_id) + "/questions"
-    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    r = requests.post(url, data=json.dumps(payload), headers=h)
     return r.json(), r.status_code
 
 
 def reply_question(publication_id, question_id, payload):
+    h = get_sv_auth_headers()
+    h.update(headers)
     url = (
         PUBLICATIONS_URL + "/" + str(publication_id) + "/questions/" + str(question_id)
     )
-    r = requests.patch(url, data=json.dumps(payload), headers=headers)
+    r = requests.patch(url, data=json.dumps(payload), headers=h)
     return r.json(), r.status_code
